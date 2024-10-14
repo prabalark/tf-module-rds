b6a6dc4544957a58dbd87d70ea1cf987be2f4024
prabala <radhakrishnaprabala8@gmail.com>
2024-10-14 22:23:23 +0530

    var.tf vr1

--- vars.tf
+++ vars.tf
@@ -1,13 +1,15 @@
 variable "vpc_id" {}
 variable "allow_db_cidr" {}
+variable "port" {
+  default = 3306
+}
+
 variable "name" {
   default = "rds"
 }
 variable "env" {}
+
 variable "engine_version" {}
-variable "port" {
-  default = 3306
-}
 variable "kms_arn" {}
 variable "tags" {}
 variable "subnets" {}
